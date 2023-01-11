2192d12b1af9f2b1e5cab0d27a867e56e1de947b
valentin-droid <valentin.drelon23@icloud.com>
2023-01-11 15:25:44 +0100

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Valentin Drelon on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
